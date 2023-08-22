790a52279b4a8826dcf5c8f20860fd8c754e6bda
shivangrawat30 <rawatshivang30@gmail.com>
2023-08-22 16:53:37 +0530

    added backend
    Signed-off-by: shivangrawat30 <rawatshivang30@gmail.com>

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,3 +1,3 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const TODO_PROGRAM_PUBKEY = new PublicKey("EMRELEkPnwDNsuyHmYZYmMroJ69FpdAfSgDHrTxEYrq3");
+export const TODO_PROGRAM_PUBKEY = new PublicKey("Cuhwz6W467HthXn1rn6zk1nsSuxdDBYZ1qwgmPNY5CM");
--- app/hooks/todo.js
+++ app/hooks/todo.js
@@ -79,20 +79,66 @@ export function useTodo() {
     }, [connection, anchorWallet])
 
     useEffect(() => {
+        // fetch user profile if there is a profile then get its TodoAccounts
+        const findProfileAccounts = async () => {
+            if (program && publicKey && !transactionPending) {
+                try {
+                    setLoading(true)
+                    const [profilePda, profileBump] = await findProgramAddressSync([utf8.encode('USER_STATE'), publicKey.toBuffer()], program.programId)
+                    const profileAccount = await program.account.userProfile.fetch(profilePda)
 
-        if(initialized) {
-            setTodos(dummyTodos)
+                    if (profileAccount) {
+                        setLastTodo(profileAccount.lastTodo)
+                        setInitialized(true)
+
+                        const todoAccounts = await program.account.todoAccount.all([authorFilter(publicKey.toString())])
+                        setTodos(todoAccounts)
+                    } else {
+                        setInitialized(false)
+                    }
+                } catch (error) {
+                    console.log(error)
+                    setInitialized(false)
+                    setTodos([])
+                } finally {
+                    setLoading(false)
+                }
+            }
         }
+        findProfileAccounts()
 
-
-    }, [initialized])
+    }, [publicKey, program, transactionPending])
 
     const handleChange = (e)=> {
         setInput(e.target.value)
     }
   
-    const initializeStaticUser = () => {
-        setInitialized(true)
+    const initializeUser = async () => {
+        // Check if the program exist and wallet is connected
+        if(program && publicKey) {
+            try{
+                setTransactionPending(true)
+                const [profilePda, profileBump] = findProgramAddressSync([utf8.encode('USER_STATE'), publicKey.toBuffer()], program.programId)
+
+                const tx = await program.methods
+                    .initializeUser()
+                    .accounts({
+                        userProfile: profilePda,
+                        authority: publicKey,
+                        systemProgram: SystemProgram.programId,
+                    })
+                    .rpc()
+                setInitialized(true)
+                toast.success('Successfully initialized user.')
+
+            } catch(error) {
+                console.log(error);
+                toast.error(error.toString())
+            } finally{
+                setTransactionPending(false);
+            }
+        }
+        // then run initializeUser() from smart contract
     }
 
     const addStaticTodo = (e) => {
@@ -146,5 +192,5 @@ export function useTodo() {
     const incompleteTodos = useMemo(() => todos.filter((todo) => !todo.account.marked), [todos])
     const completedTodos = useMemo(() => todos.filter((todo) => todo.account.marked), [todos])
 
-    return { initialized, initializeStaticUser, loading, transactionPending, completedTodos, incompleteTodos, markStaticTodo, removeStaticTodo, addStaticTodo, input, setInput, handleChange }
+    return { initialized, initializeUser, loading, transactionPending, completedTodos, incompleteTodos, markStaticTodo, removeStaticTodo, addStaticTodo, input, setInput, handleChange }
 }
